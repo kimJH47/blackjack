c5b35d29eafd584454283dc34874f3f31f5c1fe5
kimJaeHyeon <kmr2644@gmail.com>
2023-03-22 01:14:31 +0900

    feat: inputPlayers 메서드 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class InputView {
+    private static final Scanner sc = new Scanner(System.in);
+    public  List<String> inputPlayers() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        StringTokenizer stringTokenizer = new StringTokenizer(sc.nextLine(), ",");
+        ArrayList<String> playNames = new ArrayList<>();
+        while (stringTokenizer.hasMoreTokens()) {
+            playNames.add(stringTokenizer.nextToken());
+        }
+        return playNames;
+    }
+
+}

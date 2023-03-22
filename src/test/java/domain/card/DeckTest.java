08996514acec15f9a0d90044f53364dbee99b493
kimJaeHyeon <kmr2644@gmail.com>
2023-03-23 01:55:46 +0900

    test: drawFirst 테스트 작성

---
+++ src/test/java/domain/card/DeckTest.java
@@ -0,0 +1,20 @@
+package domain.card;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+    @Test
+    @DisplayName("덱 생성 후 drawFirst 시 카드 2장을 담은 리스트가 반환되어야한다.")
+    public void draw_first() throws Exception{
+        //given
+        Deck deck = Deck.create();
+        //when
+        List<Card> cards = deck.drawFirst();
+        //then
+        assertThat(cards.size()).isEqualTo(2);
+    }
+}

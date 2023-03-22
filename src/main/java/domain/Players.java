a359b09ddf037c5b8beb8802d902fb966008a377
kimJaeHyeon <kmr2644@gmail.com>
2023-03-22 21:03:44 +0900

    feat: Players 객체 name 검증 메서드 및 add 메서드 구현

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Players {
+
+    private List<Player> players = new ArrayList<>();
+    private final Deck deck;
+
+    public Players() {
+        deck = Deck.create();
+    }
+
+    public void add(List<String> playerNames) {
+        validName(playerNames);
+        playerNames.forEach(name ->
+                this.players.add(
+                        new Player(name, new Hands(deck.getFirst())))
+        );
+    }
+
+    private void validName(List<String> players) {
+        for (String player : players) {
+            if (!player.matches("^[a-zA-Z]*$")) {
+                throw new IllegalArgumentException("이름은 영어만 입력가능합니다.");
+            }
+        }
+    }
+}

dbc33229371a22d9c0523861bfa2decd840aece2
kimJaeHyeon <kmr2644@gmail.com>
2023-03-22 21:21:18 +0900

    feat: name 과 hand 객체를 의존하는 player 구현

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Player {
+    private final String name;
+    private final Hands hands;
+
+    public Player(String name, Hands hands) {
+        this.name = name;
+        this.hands = hands;
+    }
+
+}
+

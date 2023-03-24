d3ad5e4695959a297a6f274db3716c24a275293a
kimJaeHyeon <kmr2644@gmail.com>
2023-03-25 03:28:07 +0900

    test: add 메서드 테스트 작성

--- src/test/java/domain/player/HandsTest.java
+++ src/test/java/domain/player/HandsTest.java
@@ -15,7 +15,7 @@ class HandsTest {
 
     @Test
     @DisplayName("Hand 객체 생성 시 2개의 카드 리스트를 인자로 받고 생성되어야한다.")
-    public void create() throws Exception{
+    public void create() throws Exception {
         //given
         List<Card> cards = List.of(new Card(Rank.ACE, Symbol.CLOVER), new Card(Rank.TWO, Symbol.CLOVER));
         //when
@@ -26,5 +26,22 @@ class HandsTest {
         assertThat(handsStatus.getHandsStatus()).isEqualTo(HandsStatus.NONE);
 
     }
+
+    @Test
+    @DisplayName("add 메서드로 카드 추가시 추가된 카드리스트 상태가 반환되어야함")
+    void add() {
+        //given
+        List<Card> cards = List.of(new Card(Rank.ACE, Symbol.CLOVER), new Card(Rank.TWO, Symbol.CLOVER));
+        Hands hands = new Hands(cards);
+        //when
+        Card card = new Card(Rank.TEN, Symbol.SPADE);
+        HandsStatusDto actual = hands.add(List.of(card));
+        //then
+        assertThat(actual.getHandsStatus()).isEqualTo(HandsStatus.NONE);
+        assertThat(actual.getCards().size()).isEqualTo(3);
+        assertThat(actual.getCards()).contains(card.toString());
+
+    }
+
 
 }

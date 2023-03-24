fab74a52c0da96cec16c063850c45aaa3bb40011
kimJaeHyeon <kmr2644@gmail.com>
2023-03-24 18:50:13 +0900

    feat: 카드리스트 를받아서 add 하는 메서드와 상태를 반환하는 메서드 구현

---
+++ src/main/java/domain/player/Hands.java
@@ -0,0 +1,47 @@
+package domain.player;
+
+
+import domain.card.Card;
+import domain.player.dto.HandsStatusDto;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Hands {
+    private List<Card> cards;
+
+    public Hands(List<Card> cards) {
+        this.cards = new ArrayList<>(cards);
+    }
+
+    public HandsStatusDto add(List<Card> cards) {
+        this.cards.addAll(cards);
+        return createHandsStatus();
+    }
+
+    public HandsStatusDto createHandsStatus() {
+        return cards.stream()
+                .map(Card::toString)
+                .collect(Collectors.collectingAndThen(Collectors.toList(),
+                        strings -> new HandsStatusDto(strings, createScore())));
+    }
+
+    private HandsStatus createScore() {
+        return Score.calc(sumExcludeAceScore(),sumAceCount());
+    }
+
+    private int sumExcludeAceScore() {
+        return cards.stream()
+                .filter(card -> !card.isAce())
+                .mapToInt(Card::getRankScore)
+                .sum();
+    }
+
+    private int sumAceCount() {
+        return (int) cards.stream()
+                .filter(Card::isAce)
+                .count();
+    }
+}

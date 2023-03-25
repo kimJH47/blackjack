ac3412e1cf59af9b3c20422eab885015b29bedf6
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 02:48:09 +0900

    refactor: 생성시 카드 생성 메서드 분리 및 메서드명 변경

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,25 +1,25 @@
 package domain.player;
 
 import domain.card.Card;
-import domain.player.dto.playerStatusDto;
+import domain.player.dto.PlayerStatusDto;
 import java.util.List;
 
 public class Player {
     private final String name;
     private final Hands hands;
 
-    public Player(String name, List<Card> cards) {
+    public Player(String name) {
         this.name = name;
-        this.hands = new Hands(cards);
+        this.hands = new Hands();
     }
 
-    public playerStatusDto add(List<Card> cards) {
+    public PlayerStatusDto add(List<Card> cards) {
         hands.add(cards);
-        return getPlayerStatusDto();
+        return createPlayerStatusDto();
     }
 
-    private playerStatusDto getPlayerStatusDto() {
-        return new playerStatusDto(name, hands.createHandsStatus());
+    private PlayerStatusDto createPlayerStatusDto() {
+        return new PlayerStatusDto(name, hands.createHandsStatus());
     }
 
 }

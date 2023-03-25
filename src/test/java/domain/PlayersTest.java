eb808deed85d3c9a356aa72de70ce2147bcfbf05
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 02:52:08 +0900

    test: 생성 테스트로직 변경

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,8 +1,9 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import domain.player.dto.PlayerStatusDto;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,24 +11,29 @@ import org.junit.jupiter.api.Test;
 class PlayersTest {
     @Test
     @DisplayName("올바른 형식의 이름 리스트가 오면 예외 없이 플레이어가 생성되어야한다.")
-    public void crate() throws Exception{
+    public void crate() throws Exception {
         //given
         Players players = new Players();
         List<String> playerNames = List.of("asd", "poby", "glan");
         //when
+        List<PlayerStatusDto> actual = players.addPlayer(playerNames);
         //then
-        assertThatCode(() -> players.add(playerNames))
-                .doesNotThrowAnyException();
+        assertThat(actual.size()).isEqualTo(3);
+        assertThat(actual.stream()
+                .map(PlayerStatusDto::getName)
+                .allMatch(playerNames::contains))
+                .isTrue();
     }
+
     @Test
     @DisplayName("player 생성시 올바르지않은 이름형식 리스트가 오면 예외가 발생해야함")
-    public void create_ex() throws Exception{
+    public void create_ex() throws Exception {
         //given
         Players players = new Players();
         List<String> playerNames = List.of("asd", "poby!@", "g.lan");
         //when
         //then
-        assertThatThrownBy(() -> players.add(playerNames))
+        assertThatThrownBy(() -> players.addPlayer(playerNames))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

5da89af28ada94261cfcd19fff699fc657654b71
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 02:42:45 +0900

    feat: 일반 드로우 메서드 구현

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -8,6 +8,7 @@ import java.util.stream.IntStream;
 
 public class Deck {
     private final static int FIRST_DRAW_CARD_COUNT = 2;
+    private final static int NORMAL_DRAW_CARD_COUNT = 1;
     private final Queue<Card> cards;
 
     private Deck(List<Card> cards) {
@@ -17,7 +18,9 @@ public class Deck {
     public List<Card> drawFirst() {
         return draw(FIRST_DRAW_CARD_COUNT);
     }
-
+    public List<Card> drawNormal() {
+        return draw(NORMAL_DRAW_CARD_COUNT);
+    }
     private List<Card> draw(int count) {
         return IntStream.range(0, count)
                 .mapToObj(operand -> cards.remove())

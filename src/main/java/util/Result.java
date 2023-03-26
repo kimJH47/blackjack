57ae5834e83c4fd6b7aa1223d75ce54a2e216780
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 18:49:31 +0900

    feat: point 필드 구현

--- src/main/java/util/Result.java
+++ src/main/java/util/Result.java
@@ -1,7 +1,16 @@
 package util;
 
 public enum Result {
-    WIN,
-    LOSE,
-    DEFAULT
+    WIN(1),
+    LOSE(0),
+    DEFAULT(0);
+    private final int point;
+
+    Result(int point) {
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
 }

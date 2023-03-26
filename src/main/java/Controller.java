c8a1a332617c74c17a5bb52849481a965676b446
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 20:57:08 +0900

    feat: player add 메서드 구현

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,5 +1,10 @@
+import domain.player.dto.PlayerStatusDto;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 import view.InputView;
 import view.OutputView;
+import view.SelectType;
 
 public class Controller {
     private final BlackjackGame blackjackGame;
@@ -14,9 +19,29 @@ public class Controller {
 
     public void run() {
         addPlayers();
+        stayOrHit();
+        result();
     }
 
     private void addPlayers() {
-        blackjackGame.addPlayer(inputView.inputPlayers());
+        List<PlayerStatusDto> playerStatusDtos = blackjackGame.addPlayer(inputView.inputPlayers());
+        outputView.firstDrawResult(playerStatusDtos);
+    }
+
+    private void stayOrHit() {
+        //하는중
+        BlackJackResult blackJackResult = blackjackGame.processDealerAndResult();
+        Stream<SelectType> selectTypeStream = blackJackResult.getPariticipants().stream()
+                .map(PlayerStatusDto::getName)
+                .map(inputStayOrHit());
+    }
+
+    private Function<String, SelectType> inputStayOrHit() {
+        return inputView::inputHitOrStay;
+    }
+
+    private void result() {
+
     }
 }
+

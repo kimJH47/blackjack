1997fea63de7da868f386066ddc9b936d0fbdd25
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 19:34:58 +0900

    feat: 결과 반환 객체 구현

--- src/main/java/BlackJackResult.java
+++ src/main/java/BlackJackResult.java
@@ -1,14 +1,28 @@
 import domain.player.dto.PlayerStatusDto;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class BlackJackResult {
 
     private final PlayerStatusDto dealer;
     private final List<PlayerStatusDto> pariticipants;
-
-    public BlackJackResult(PlayerStatusDto dealer, List<PlayerStatusDto> participants) {
+    private final Map<String, Integer> dashBoard;
+    public BlackJackResult(PlayerStatusDto dealer, List<PlayerStatusDto> participants, Map<String,Integer> dashBoard) {
         this.dealer = dealer;
         this.pariticipants = new ArrayList<>(participants);
+        this.dashBoard = dashBoard;
+    }
+
+    public PlayerStatusDto getDealer() {
+        return dealer;
+    }
+
+    public List<PlayerStatusDto> getPariticipants() {
+        return pariticipants;
+    }
+
+    public Map<String, Integer> getDashBoard() {
+        return dashBoard;
     }
 }

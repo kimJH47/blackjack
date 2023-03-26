83b8802eec75ee71e0b2c88b934af39fa77f258e
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 20:56:44 +0900

    feat: 첫번째 드로우 결과 메서드 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,23 @@
 package view;
 
+import domain.player.dto.PlayerStatusDto;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+
+    // private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    public void firstDrawResult(List<PlayerStatusDto> playerStatusDtos) {
+        System.out.printf("딜러와 %s 에게 카드 2장씩 나누어 주었습니다.", formattingPlayerNames(playerStatusDtos));
+        playerStatusDtos.stream().map(PlayerStatusDto::toInfo).forEach(System.out::println);
+    }
+
+    private String formattingPlayerNames(List<PlayerStatusDto> playerStatusDtos) {
+        return playerStatusDtos.stream()
+                .map(PlayerStatusDto::getName)
+                .collect(Collectors.joining(","));
+
+    }
+
 }

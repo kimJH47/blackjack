c9458e94e7280b7ee3343bbe1242f94d909c4ace
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 20:43:08 +0900

    test: 결과반환 테스트 작성

--- src/test/java/BlackjackGameTest.java
+++ src/test/java/BlackjackGameTest.java
@@ -1,9 +1,18 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import domain.card.Card;
+import domain.card.Deck;
+import domain.card.Rank;
 import domain.card.ShuffleDeck;
+import domain.card.Symbol;
+import domain.player.Dealer;
 import domain.player.dto.PlayerStatusDto;
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -52,6 +61,62 @@ class BlackjackGameTest {
         //then
         assertThat(expected.getName()).isEqualTo(actual);
         assertThat(expected.getHandsStatusDto().getCards().size()).isEqualTo(3);
+
+    }
+
+    @Test
+    @DisplayName("딜러와 참가자의 카드를 확인하여 게임결과를 반환해야한다.")
+    public void processDealerAndResult() throws Exception {
+        //given
+        List<Card> cards = List.of(
+                new Card(Rank.ACE, Symbol.CLOVER), new Card(Rank.FIVE, Symbol.CLOVER),
+
+                new Card(Rank.ACE, Symbol.HEART), new Card(Rank.JACK, Symbol.HEART),
+
+                new Card(Rank.ACE, Symbol.CLOVER)
+
+        );
+
+        String actual = "kim";
+        List<String> playerNames = List.of(actual);
+
+        BlackjackGame blackjackGame = new BlackjackGame(new TestDeck(cards));
+        blackjackGame.addPlayer(playerNames);
+        //when
+        BlackJackResult blackJackResult = blackjackGame.processDealerAndResult();
+        PlayerStatusDto dealer = blackJackResult.getDealer();
+        List<PlayerStatusDto> pariticipants = blackJackResult.getPariticipants();
+        //then
+        assertThat(dealer.getName()).isEqualTo(Dealer.NAME);
+        assertThat(dealer.getScore()).isEqualTo(17);
+        assertThat(dealer.getHandsStatusDto().getCards().size()).isEqualTo(3);
+        assertThat(pariticipants.get(0).getScore()).isEqualTo(21);
+        assertThat(pariticipants.get(0).getName()).isEqualTo(actual);
+
+    }
+
+    static class TestDeck implements Deck {
+        private final Queue<Card> cards;
+
+        public TestDeck(List<Card> cards) {
+            this.cards = new ArrayDeque<>(cards);
+        }
+
+        @Override
+        public List<Card> drawFirst() {
+            return draw(2);
+        }
+
+        @Override
+        public List<Card> drawNormal() {
+            return draw(1);
+        }
+
+        private List<Card> draw(int count) {
+            return IntStream.range(0, count)
+                    .mapToObj(operand -> cards.remove())
+                    .collect(Collectors.toList());
+        }
 
     }
 }

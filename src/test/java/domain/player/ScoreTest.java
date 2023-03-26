061753d6e27d4f20b33a9a3847b22ea6e62b84ab
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 13:10:01 +0900

    test: 테스트 케이스 변경

--- src/test/java/domain/player/ScoreTest.java
+++ src/test/java/domain/player/ScoreTest.java
@@ -13,27 +13,27 @@ class ScoreTest {
     @ParameterizedTest
     @MethodSource("provideSumExcludeAceScoreAndsAceCount")
     @DisplayName("각 점수에 해당하는 HandStatus 가 생성되어야한다")
-    public void create(int sum, int aceCount, HandsStatus expected) throws Exception {
+    public void create(int sum, int aceCount, int expected) throws Exception {
         //given
         //when
-        HandsStatus actual = Score.calc(sum, aceCount);
+        int actual = Score.calc(sum, aceCount);
         //then
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
     private static Stream<Arguments> provideSumExcludeAceScoreAndsAceCount() {
         return Stream.of(
-                Arguments.of(10, 1,HandsStatus.BLACK_JACK),
-                Arguments.of(20, 1,HandsStatus.BLACK_JACK),
-                Arguments.of(21, 0,HandsStatus.BLACK_JACK),
-                Arguments.of(20, 2,HandsStatus.BUST),
-                Arguments.of(22, 0,HandsStatus.BUST),
-                Arguments.of(10, 2,HandsStatus.NONE),
-                Arguments.of(9, 1,HandsStatus.NONE),
-                Arguments.of(1, 1,HandsStatus.NONE),
-                Arguments.of(5, 3,HandsStatus.NONE),
-                Arguments.of(0, 4,HandsStatus.NONE),
-                Arguments.of(0, 2,HandsStatus.NONE)
+                Arguments.of(10, 1, 21),
+                Arguments.of(20, 1, 21),
+                Arguments.of(21, 0, 21),
+                Arguments.of(20, 2, 22),
+                Arguments.of(22, 0, 22),
+                Arguments.of(10, 2, 12),
+                Arguments.of(9, 1, 20),
+                Arguments.of(1, 1, 12),
+                Arguments.of(5, 3, 18),
+                Arguments.of(0, 4, 14),
+                Arguments.of(0, 2, 12)
         );
     }
 

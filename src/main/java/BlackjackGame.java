991193846b153ef226c34e9ed6f084057c3512e7
kimJaeHyeon <kmr2644@gmail.com>
2023-03-26 18:49:07 +0900

    feat: 딜러 연산 및 결과 게임 반환 메서드 구현

--- src/main/java/BlackjackGame.java
+++ src/main/java/BlackjackGame.java
@@ -1,6 +1,11 @@
 import domain.Players;
+import domain.player.Dealer;
 import domain.player.dto.PlayerStatusDto;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import util.Result;
+import util.Score;
 
 public class BlackjackGame {
 
@@ -20,6 +25,30 @@ public class BlackjackGame {
 
     public PlayerStatusDto addCard(String name) {
         return players.addCard(name);
+    }
+
+    public String processDealerAndResult() {
+        if (!players.isStayDealer()) {
+            drawDealerCardAndUpdate();
+        }
+
+        PlayerStatusDto dealer = players.getDealerStatusDto();
+        List<PlayerStatusDto> participants = players.getParticipantsStatusDto();
+        return participants.stream()
+                .map(playerStatusDto -> createResult().apply(playerStatusDto, dealer))
+                .collect(Collectors.joining("\n"));
+    }
+
+    private BiFunction<PlayerStatusDto, PlayerStatusDto, String> createResult() {
+        return (dealer, playerStatusDto) -> {
+            Result result = Score.getResult(dealer, playerStatusDto);
+            dashBoard.updateResult(playerStatusDto.getName(), result);
+            return String.format(playerStatusDto.toInfo() + " 결과: " + playerStatusDto.getScore() + "\n");
+        };
+    }
+
+    private void drawDealerCardAndUpdate() {
+        players.addCard(Dealer.NAME);
     }
 
 }

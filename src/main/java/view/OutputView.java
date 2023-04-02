5c67e72c07fba4b6ca2ca5e21c4c8135e3e30558
kimJaeHyeon <kmr2644@gmail.com>
2023-04-02 21:25:50 +0900

    refactor: dto 메서드 내부구현으로 인해 출력로직 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,33 +1,29 @@
 package view;
 
 import domain.BlackJackResult;
-import domain.player.dto.PlayerStatusDto;
+import domain.dto.PlayerStatusDto;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class OutputView {
 
     // private static final String LINE_SEPARATOR = System.lineSeparator();
 
     public void firstDrawResult(BlackJackResult blackJackResult) {
-        List<PlayerStatusDto> participants = blackJackResult.getParticipants();
-        System.out.printf("딜러와 %s 에게 카드 2장씩 나누어 주었습니다.\n", formattingPlayerNames(participants));
-        PlayerStatusDto dealer = blackJackResult.getDealer();
-        System.out.println(dealer.toInfo());
-        participants.stream().map(PlayerStatusDto::toInfo).forEach(System.out::println);
+        System.out.printf("딜러와 %s 에게 카드 2장씩 나누어 주었습니다.\n",
+                formattingPlayerNames(blackJackResult.getParticipantNames()));
+        System.out.println(blackJackResult.getDealerInfo());
+        List<String> playersInfo = blackJackResult.getPlayersInfo();
+        playersInfo.forEach(System.out::println);
     }
 
     public void hitResult(PlayerStatusDto playerStatusDto) {
         System.out.println(playerStatusDto.toInfo());
     }
 
-
     public void printResult(BlackJackResult result) {
         System.out.println(result.getDealer().toInfo());
-        result.getParticipants().stream()
-                .map(PlayerStatusDto::toInfo)
-                .forEach(System.out::println);
+        result.getParticipantNames().forEach(System.out::println);
         Map<String, Integer> dashBoard = result.getDashBoard();
         System.out.println(dashBoard);
     }
@@ -36,10 +32,7 @@ public class OutputView {
         System.out.println(massage);
     }
 
-    private String formattingPlayerNames(List<PlayerStatusDto> playerStatusDtos) {
-        return playerStatusDtos.stream()
-                .map(PlayerStatusDto::getName)
-                .collect(Collectors.joining(","));
-
+    private String formattingPlayerNames(List<String> participantNames) {
+        return String.join(",", participantNames);
     }
 }

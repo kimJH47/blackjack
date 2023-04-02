b2c40a79bbf651a4c990ccda100ff12d9b034c5f
kimJaeHyeon <kmr2644@gmail.com>
2023-04-02 20:19:23 +0900

    feat: isHit 구현

--- src/main/java/view/SelectType.java
+++ src/main/java/view/SelectType.java
@@ -10,13 +10,18 @@ public enum SelectType {
     }
 
     private final String select;
+
     public static SelectType of(String select) {
-        if (select.toUpperCase().equals("y")) {
+        if (select.equalsIgnoreCase("y")) {
             return HIT;
         }
-        if (select.toUpperCase().equals("n")) {
+        if (select.equalsIgnoreCase("n")) {
             return STAY;
         }
         throw new IllegalArgumentException("y 또는 n 을 입력해야합니다.");
+    }
+
+    public boolean isHit() {
+        return select.equals("y");
     }
 }

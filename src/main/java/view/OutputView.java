d19ffccd46d2eaa51ca7cbb8fe711b9337ceaa63
kimJaeHyeon <kmr2644@gmail.com>
2023-04-02 20:27:23 +0900

    feat: output 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,16 +1,39 @@
 package view;
 
+import domain.BlackJackResult;
 import domain.player.dto.PlayerStatusDto;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
 
     // private static final String LINE_SEPARATOR = System.lineSeparator();
 
-    public void firstDrawResult(List<PlayerStatusDto> playerStatusDtos) {
-        System.out.printf("딜러와 %s 에게 카드 2장씩 나누어 주었습니다.", formattingPlayerNames(playerStatusDtos));
-        playerStatusDtos.stream().map(PlayerStatusDto::toInfo).forEach(System.out::println);
+    public void firstDrawResult(BlackJackResult blackJackResult) {
+        List<PlayerStatusDto> participants = blackJackResult.getParticipants();
+        System.out.printf("딜러와 %s 에게 카드 2장씩 나누어 주었습니다.\n", formattingPlayerNames(participants));
+        PlayerStatusDto dealer = blackJackResult.getDealer();
+        System.out.println(dealer.toInfo());
+        participants.stream().map(PlayerStatusDto::toInfo).forEach(System.out::println);
+    }
+
+    public void hitResult(PlayerStatusDto playerStatusDto) {
+        System.out.println(playerStatusDto.toInfo());
+    }
+
+
+    public void printResult(BlackJackResult result) {
+        System.out.println(result.getDealer().toInfo());
+        result.getParticipants().stream()
+                .map(PlayerStatusDto::toInfo)
+                .forEach(System.out::println);
+        Map<String, Integer> dashBoard = result.getDashBoard();
+        System.out.println(dashBoard);
+    }
+
+    public void printException(String massage) {
+        System.out.println(massage);
     }
 
     private String formattingPlayerNames(List<PlayerStatusDto> playerStatusDtos) {
@@ -19,5 +42,4 @@ public class OutputView {
                 .collect(Collectors.joining(","));
 
     }
-
 }
